6dd769abfd5d02c7844247498c08353e530dda1c
Jere Vahlman <jere.vahlman@solita.fi>
2024-12-01 10:22:47 +0200

    Day1 solutions

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,6 +1,7 @@
 use anyhow::*;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::ops::Index;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
 use adv_code_2024::*;
@@ -23,9 +24,41 @@ fn main() -> Result<()> {
     //region Part 1
     println!("=== Part 1 ===");
 
-    fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        // TODO: Solve Part 1 of the puzzle
-        let answer = reader.lines().flatten().count();
+    fn part1<R: BufRead>(reader: R) -> Result<i128> {
+
+        // start by getting two lists from input
+        let mut left_list: Vec<i128>  = vec![];
+        let mut right_list: Vec<i128>  = vec![];
+
+        // read lists
+        for line in reader.lines() {
+            let line = line?;
+            let words: Vec<&str> = line.split_whitespace().collect();
+            assert_eq!(2, words.len());
+            
+            //debug
+            //println!("{}, {}", words[0], words[1]);
+            
+            left_list.push(words[0].parse().unwrap());
+            right_list.push(words[1].parse().unwrap());
+        }
+
+        // sort lists
+        left_list.sort();
+        right_list.sort();
+
+        // debug
+        //println!("{:?}", left_list);
+        //println!("{:?}", right_list);
+
+        let mut answer: i128 = 0;
+
+        // iterate lists, subtracting r value from l, getting posive out and summing it all up 
+        for it in left_list.iter_mut().zip(right_list.iter_mut()) {
+            let (l, r) = it;
+
+            answer += (*l - *r).abs();
+        }
 
         Ok(answer)
     }
@@ -33,22 +66,50 @@ fn main() -> Result<()> {
     assert_eq!(11, part1(BufReader::new(TEST.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    let result = time_snippet!(part1(input_file)?);
+    let result: i128 = time_snippet!(part1(input_file)?);
     println!("Result = {}", result);
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+    
+    fn part2<R: BufRead>(reader: R) -> Result<i128> {
+
+        // start by getting two lists from input
+        let mut left_list: Vec<i128>  = vec![];
+        let mut right_list: Vec<i128>  = vec![];
+
+        // read lists
+        for line in reader.lines() {
+            let line = line?;
+            let words: Vec<&str> = line.split_whitespace().collect();
+            assert_eq!(2, words.len());
+            
+            //debug
+            //println!("{}, {}", words[0], words[1]);
+            
+            left_list.push(words.index(0).parse().unwrap());
+            right_list.push(words.index(1).parse().unwrap());
+        }
+
+        let mut answer: i128 = 0;
+
+        // iterate lists, subtracting r value from l, getting posive out and summing it all up 
+        for i in left_list.iter() {
+
+            let temp = i * right_list.iter().filter(|&n| *n == *i).count() as i128;
+
+            answer += temp;
+        }
+
+        Ok(answer)
+    }
+    
+    assert_eq!(31, part2(BufReader::new(TEST.as_bytes()))?);
+    
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
